cbe33fb6fa85bee7e1cb5170434bc775ea7db6f6
Sonika <141132744+sonikaa18@users.noreply.github.com>
2024-04-01 23:07:33 +0530

    Create band_name_generator.py

---
+++ band_name_generator.py
@@ -0,0 +1,9 @@
+print("Welcome to band name generator")
+# taking name of city as an input from the user
+city_name=input("What's the name of city you grew up in?\n")
+print(city_name)
+# taking name of pet as an input from the user
+pet_name=input("What's your pet name?\n")
+print(pet_name)
+# printing band name by making use of String concatenation
+print("your band name could be :"+city_name+" "+pet_name)
